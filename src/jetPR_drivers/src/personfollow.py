1fb1f17201f138cca62447ad659ab4e4dc3d2b0c
srike27 <srike27@gmail.com>
2020-01-18 22:19:43 +0530

    added person following

--- src/jetPR_drivers/src/personfollow.py
+++ src/jetPR_drivers/src/personfollow.py
@@ -11,38 +11,50 @@ from std_msgs.msg import String
 from vision_msgs.msg import Detection2DArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import UInt16
+from geometry_msgs.msg import Twist
 
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("boxed",Image,queue_size = 10)
-
     self.bridge = CvBridge()
     self.detectsub = rospy.Subscriber("detectnet/detections",Detection2DArray,self.dcbk)
+    self.servopub = rospy.Publisher("servo",UInt16,queue_size = 10)
+    self.vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size = 10)
     self.image_sub = rospy.Subscriber("imagee",Image,self.callback)
+    self.cmd_vel = Twist()
     self.cv_image = np.zeros((640,480,3), np.uint8)
     self.final_img = np.zeros((640,480,3), np.uint8)
+    self.servo = UInt16()
+    self.flag = 0
 
   def dcbk(self,msg):
     for det in msg.detections:
-      if det.results[0].id == 1 and det.results[0].score > 0.75 :
-        pt1 = (int(det.bbox.center.x - det.bbox.size_x/2),int(det.bbox.center.y - det.bbox.size_y/2))
-        pt2 = (int(det.bbox.center.x + det.bbox.size_x/2),int(det.bbox.center.y + det.bbox.size_y/2))
-        self.final_img = cv2.rectangle(self.cv_image, pt1, pt2, color=(0, 255, 0), thickness=3)
+      if det.results[0].id == 1 and det.results[0].score > 0.5 :
+        if det.bbox.size_y == 400 and self.servo.data > 0:
+          self.servo.data -= 1
+        elif det.bbox.center.y > 240 and self.servo.data < 180:
+          self.servo.data += 1
+	elif det.bbox.center.y <240 and self.servo.data > 0:
+          self.servo.data -= 1
+        self.cmd_vel.angular.z = -0.2*(det.bbox.center.x - 320)/80
+	area = det.bbox.size_x*det.bbox.size_y
+        self.cmd_vel.linear.x = 0.1*(area - 76800)/76800
       else:
-        self.final_img = self.cv_image
+        self.cmd_vel.angular.z = 0
+      self.vel_pub.publish(self.cmd_vel)
+      self.servopub.publish(self.servo)
 
   def callback(self,data):
+    if self.flag == 0:
+       self.servo.data = 20
+       self.servopub.publish(self.servo)
+       self.flag = 1
     try:
       self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-    cv2.imshow('bounding_box',self.final_img)
     cv2.waitKey(3)
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(self.final_img, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 def main(args):
   ic = image_converter()
